88e87970198adef7de1e1e321a580818d5193c39
MD ABDULLAH <abdullah_bd333@outlook.com>
2020-04-17 22:33:51 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2*i-1; k++)
+                {
+                    Console.Write("*" + "");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Length of the Pyramid");
+            DrawPyramid(Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}
